43c19adf9005d475071d3ab009a977686e5c1113
Victor Hayafuji <99742780+vichayafuji@users.noreply.github.com>
2022-06-09 01:01:50 -0300

    Add files via upload

---
+++ Projeto_Final/site/src/database/script-tabelas.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE acquatec;
+
+USE acquatec;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+	build char(15)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
